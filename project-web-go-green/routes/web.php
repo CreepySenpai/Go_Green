185241fca9ae10a936bc9ee5412a5dbf39b86cc8
Baotq1406 <130818672+Baotq1406@users.noreply.github.com>
2023-11-11 11:13:04 +0700

    show product

--- project-web-go-green/routes/web.php
+++ project-web-go-green/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\Customer\ShopController;
 use App\Livewire\AddProductComponent;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontController;
@@ -38,4 +39,4 @@ Route::group(['namespace' => 'Admin'], function() {
 });
 
 //Group Route In Folder Customer
-Route::get('/', [FrontController::class, 'index'])->name('front.shop');
+Route::get('/shop', [ShopController::class, 'index'])->name('front.shop');
